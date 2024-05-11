5f88ed3bc574b32eaaf300edc6d1fea2cf00878f
Ali <109759559+AliAhmed2004@users.noreply.github.com>
2024-05-10 22:00:34 -0400

    project section changes

--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import stock from "./stock.jpg";
@@ -6,10 +6,28 @@ import ai from "./ai.jpg";
 import hub from "./hub.jpg";
 
 const Projects = () => {
+  const scrollContainerRef = useRef(null);
+
+  useEffect(() => {
+    const container = scrollContainerRef.current;
+
+    console.log("Initial scrollLeft:", container.scrollLeft);
+
+    container.scrollLeft = 0;
+
+    setTimeout(() => {
+      console.log("Delayed scrollLeft:", container.scrollLeft);
+    }, 500);
+  }, []);
+
   return (
-    <div className="projects-container">
+    <div ref={scrollContainerRef} className="projects-container">
       <div className="project-card">
-        <img src={stock} alt="Project Name" className="project-image" />
+        <img
+          src={stock}
+          alt="Twitter Stock Analysis"
+          className="project-image"
+        />
         <div className="project-info">
           <h3>Twitter Stock Analysis</h3>
           <p>A machine learning model used to analyze Twitter stocks</p>
@@ -23,7 +41,7 @@ const Projects = () => {
         </div>
       </div>
       <div className="project-card">
-        <img src={ai} alt="Project Name" className="project-image" />
+        <img src={ai} alt="AI Image Generator" className="project-image" />
         <div className="project-info">
           <h3>AI Image Generator</h3>
           <p>
@@ -40,7 +58,11 @@ const Projects = () => {
         </div>
       </div>
       <div className="project-card">
-        <img src={hub} alt="Project Name" className="project-image" />
+        <img
+          src={hub}
+          alt="Laurier Student Study Hub"
+          className="project-image"
+        />
         <div className="project-info">
           <h3>Laurier Student Study Hub</h3>
           <p>
